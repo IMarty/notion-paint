5195f34817b57ebce402970bf540bf93ba88ca94
Brian Zindler <zindlerb@gmail.com>
2016-10-28 21:05:24 -0700

    working menu need to clean up code

--- server/public/test.js
+++ server/public/test.js
@@ -17,35 +17,32 @@ function makeValidSelector(str) {
     return "cl_" + str.replace("#", "");
 }
 
-
+// from http://bl.ocks.org/eesur/4e0a69d57d3bfc8a82c2
+d3.selection.prototype.moveToFront = function() {  
+    return this.each(function(){
+        this.parentNode.appendChild(this);
+    });
+};
 
 var Menu = function() {
+    var size = 150;
     var menuAttrs = {
-        height: 120,
-        width: 120
+        height: size,
+        width: size
     }
-    
-    var menuElement = createMenu();
+
     var activeSections =  {
         size: "small",
         color: "black"
     };
 
+    var menuElement = createMenu();
+
     function createMenu() {
-        var body = d3.select("body");
+        // Data
+        var MENU_CHILD_CLASS = "menuChildClass";
         var width = menuAttrs.width;
         var height = menuAttrs.height;
-        
-        var svg = body.append("svg")
-                      .attr("style", style({display: "none"}))
-                      .attr("width", width)
-                      .attr("height", height);
-
-        var menu = svg.append("g")
-                      .attr("transform", translate(width/2, height/2))
-
-        var exit = svg.append("g")
-                      .attr("transform", translate(width/2, height/2));
 
         var exitRadius = 20;
         var outerRadius = 60;
@@ -97,11 +94,68 @@ var Menu = function() {
 
         var menuData = colorData.concat(sizeData, actionData);
 
+        // Generators
         var exitArc = d3.arc();
         var menuArc = d3.arc()
                         .innerRadius(exitRadius)
                         .outerRadius(outerRadius);
 
+        var pie = d3.pie()
+                    .sort(null)
+                    .value(function(d) { return d.size });
+
+
+        // Create Top Level Elements
+        var body = d3.select("body");
+        var svg = body.append("svg")
+                      .attr("style", style({display: "none"}))
+                      .attr("width", width)
+                      .attr("height", height);
+
+        var menu = svg.append("g")
+                      .attr("transform", translate(width/2, height/2))
+
+        var exit = svg.append("g")
+                      .attr("transform", translate(width/2, height/2));
+
+
+        // Adds filter for drop shadow
+
+        // From http://bl.ocks.org/cpbotha/5200394
+        // filters go in defs element
+        var defs = svg.append("defs");
+
+        // create filter with id #drop-shadow
+        // height=130% so that the shadow is not clipped
+        var filter = defs.append("filter")
+                         .attr("id", "shadow")
+                         .attr("height", "130%");
+
+        // SourceAlpha refers to opacity of graphic that this filter will be applied to
+        // convolve that with a Gaussian with standard deviation 3 and store result
+        // in blur
+        filter.append("feGaussianBlur")
+              .attr("in", "SourceAlpha")
+              .attr("stdDeviation", 2)
+              .attr("result", "blur");
+
+        // translate output of Gaussian blur to the right and downwards with 2px
+        // store result in offsetBlur
+        filter.append("feOffset")
+              .attr("in", "blur")
+              .attr("dx", 0)
+              .attr("dy", 0)
+              .attr("result", "offsetBlur");
+
+        // overlay original SourceGraphic over translated blurred opacity by using
+        // feMerge filter. Order of specifying inputs is important!
+        var feMerge = filter.append("feMerge");
+
+        feMerge.append("feMergeNode")
+               .attr("in", "offsetBlur")
+        feMerge.append("feMergeNode")
+               .attr("in", "SourceGraphic");
+        
         
         function renderIconAtCentroid(pieDatum, iconLink) {
             var centroidPoint = menuArc.centroid(pieDatum);
@@ -115,11 +169,10 @@ var Menu = function() {
                 .attr("href", iconLink)
                 .attr("x", function (d) { return centroidPoint[0] - (iconWidth/2) })
                 .attr("y", function (d) { return centroidPoint[1] - (iconHeight/2) })
-                .attr("class", "actions");
-            
+                .attr("class", "actions");            
         }
 
-
+        // Inner exit button
         exit.selectAll("path.exit")
             .data(exitData)
             .enter()
@@ -127,76 +180,8 @@ var Menu = function() {
             .attr("d", exitArc)
             .attr("fill", function(obj) {
                 return obj.fill;
-            });
-
-        var pie = d3.pie()
-                    .sort(null)
-                    .value(function(d) { return d.size });
-
-        menu.selectAll("path.menu")
-            .data(pie(menuData))
-            .enter()
-            .append("path")
-            .attr("d", menuArc)
-            .attr("class", function (obj) {
-                return makeValidSelector(obj.data.id);
             })
-            .attr("fill", function(obj) {
-                return obj.data.fill;
-            })
-            .on("click", function(obj) {
-                // Handle Events
-                console.log("click");
-                if (obj.data.tag === "action") {
-                    return false;
-                }
-
-                activeSections[obj.data.tag] = obj.data.id;
-                d3.selectAll(".active")
-                  .classed("active", false);
-                
-                _.forEach(activeSections, function (cl, tag) {
-                    d3.select("." + makeValidSelector(cl)).classed("active", true);
-                });
-            });
-
-        var undoDatum = pie(menuData).find(
-            function(val) {
-                return val.data.id === "undo"
-            });
-
-        var redoDatum = pie(menuData).find(
-            function(val) {
-                return val.data.id === "redo"
-            });
-
-        var sizeData = pie(menuData).filter(function(menuDatum) {
-            return menuDatum.data.tag === "size";
-        });
-
-        menu.selectAll("circle.size")
-            .data(sizeData)
-            .enter()
-            .append("circle")
-            .attr("cx", function (d) {
-                return menuArc.centroid(d)[0];
-            })
-            .attr("cy", function (d) {
-                return menuArc.centroid(d)[1];
-            })
-            .attr("r", function (d) {
-                if (d.data.id === "tiny") {
-                    return 2
-                } else if (d.data.id === "small") {
-                    return 4
-                } else if (d.data.id === "medium") {
-                    return 7
-                } else if (d.data.id === "large") {
-                    return 12
-                }
-            })
-            .attr("fill", "gray")
-            .attr("style", style({"pointer-events": "none"}))
+            .on("click", hideMenu);
 
         exit.append("image")
             .attr("width", 20)
@@ -204,9 +189,114 @@ var Menu = function() {
             .attr("href", "/exit.svg")
             .attr("x", 0 - (20/2))
             .attr("y", 0 - (20/2))
+            .attr("style", style({"pointer-events": "none"}));
 
-        renderIconAtCentroid(undoDatum, '/undo.svg');
-        renderIconAtCentroid(redoDatum, '/redo.svg');
+        menu.selectAll("path.menu")
+            .data(pie(menuData))
+            .enter()
+            .append("g")
+            .attr("class", function (obj) {
+                return makeValidSelector(obj.data.id);
+            })
+            .append("path")
+            .attr("d", menuArc)            
+            .attr("fill", function(obj) {
+                return obj.data.fill;
+            })
+            .on("click", function(obj) {
+                // Handle Events
+                if (obj.data.tag === "action") {
+                    return false;
+                }
+
+                activeSections[obj.data.tag] = obj.data.id;
+                d3.selectAll(".active")
+                  .classed("active", false)
+                  .style("filter", null);
+
+                activateActiveSections(activeSections);
+            });
+
+        function activateActiveSections(activeSections) {
+            _.forEach(activeSections, function (cl, tag) {
+                d3.select("." + makeValidSelector(cl))
+                  .classed("active", true)
+                  .style("filter", "url(#shadow)")
+                  .moveToFront();
+            });    
+        }
+
+        var sizeData = pie(menuData).forEach(function(menuDatum) {
+            var d = menuDatum;
+            if(menuDatum.data.tag === "size") {
+                d3.select("g." + makeValidSelector(d.data.id))
+                  .append("circle")
+                  .attr("cx", function (d) {
+                      return menuArc.centroid(d)[0];
+                  })
+                  .attr("cy", function (d) {
+                      return menuArc.centroid(d)[1];
+                  })
+                  .attr("r", function (d) {
+                      if (d.data.id === "tiny") {
+                          return 2
+                      } else if (d.data.id === "small") {
+                          return 4
+                      } else if (d.data.id === "medium") {
+                          return 7
+                      } else if (d.data.id === "large") {
+                          return 12
+                      }
+                  })
+                  .attr("fill", "gray")
+                  .attr("style", style({"pointer-events": "none"}))        
+            }
+        });
+
+        //Render undo icon
+        renderIconAtCentroid(pie(menuData).find(function(val) {
+            return val.data.id === "undo";
+        }), '/undo.svg');
+
+        renderIconAtCentroid(pie(menuData).find(function(val) {
+            return val.data.id === "redo";
+        }), '/redo.svg');
+
+        // Draw border
+        menu.append("circle")
+            .attr("r", outerRadius)
+            .attr("class", "menuBorder");
+
+        // Draw segments
+        menu.selectAll("path.segments")
+            .data([
+                { points:[[-exitRadius, 0], [-outerRadius, 0]] },
+                { points:[[exitRadius, 0], [outerRadius, 0]] },
+                { points:[[0, -exitRadius], [0, -outerRadius]] }
+            ])
+            .enter()
+            .append("path")
+            .attr("d", function(d) { return d.points.reduce(function(str, point, ind) {
+                var nextStr = '';
+                if (ind === 0) {
+                    nextStr = "M " + point[0] + " " + point[1];
+                } else {
+                    nextStr = "L " + point[0] + " " + point[1];
+                }
+                return str + " " + nextStr;
+            }, "")})
+            .attr("stroke", "gray")
+            .attr("stroke-width", 1)
+
+        function reorder(parent) {
+            console.log(parent);
+            parent.sort(function () {
+                console.log(arguments);
+            });
+        }
+        
+        activateActiveSections(activeSections);
+        reorder(menu);
 
         return svg;
     }
@@ -247,21 +337,19 @@ function stopEvent(event){
 
 var menu = Menu();
 
-var sizeToPixels = {
-    tiny: 3,
-    small: 8,
-    medium: 16,
-    large: 64
-};
-
 d3.select("body").on("contextmenu", function() {
     var e = d3.event;
     stopEvent(e);
     return false;
 });
 
-var c = document.getElementById('canvas');
-var ctx = c.getContext("2d");
+var frontCanvasEl = document.getElementById('frontCanvas');
+var backCanvasEl = document.getElementById('backCanvas');
+
+var frontCtx = frontCanvasEl.getContext("2d");
+var backCtx = backCanvasEl.getContext("2d");
+
+var points = [];
 
 function drawCircle(ctx, cx, cy, r, color) {
     ctx.beginPath();
@@ -272,21 +360,16 @@ function drawCircle(ctx, cx, cy, r, color) {
 
 var mouseIsDown = false;
 
-d3.select(c).on("mousemove", function() {
+d3.select(frontCanvasEl).on("mousemove", function() {
+    var e = d3.event;
     if (mouseIsDown) {
-        var e = d3.event;
-        drawCircle(
-            ctx,
-            e.clientX,
-            e.clientY,
-            sizeToPixels[menu.activeSections.size],
-            menu.activeSections.color
-        );
+        points.push([e.clientX, e.clientY]);
+        drawLineToCanvas(frontCtx, points, menu, true);
     }
 });
 
 
-d3.select(c).on("mousedown", function() {
+d3.select(frontCanvasEl).on("mousedown", function() {
     var e = d3.event;
     
     var isRightMB;
@@ -296,44 +379,96 @@ d3.select(c).on("mousedown", function() {
         isRightMB = e.which == 3; 
     else if ("button" in e)  // IE, Opera 
         isRightMB = e.button == 2;
-
     if (isRightMB) {
         menu.open(e.clientX, e.clientY);
     } else if (menu.isOpen) {
         menu.close();
     } else {
         mouseIsDown = true;
-        drawCircle(
-            ctx,
-            e.clientX,
-            e.clientY,
-            sizeToPixels[menu.activeSections.size],
-            menu.activeSections.color
-        );
+        points.push([e.clientX, e.clientY]);
+        drawLineToCanvas(frontCtx, points, menu, true);
     }
 });
 
-d3.select(c).on("mouseup", function() {
+d3.select(frontCanvasEl).on("mouseup", function() {
     mouseIsDown = false;
+    drawLineToCanvas(backCtx, points, menu, false);
+    points = [];
 });
 
+function drawLineToCanvas(ctx, linePoints, menu, shouldClear) {
+    if (shouldClear) {
+        ctx.clearRect(0, 0, $(document).width(), $(document).height());
+    }
+
+    var sizeToPixels = {
+        tiny: 3,
+        small: 8,
+        medium: 16,
+        large: 64
+    };
+
+    var attrs = {
+        strokeStyle: menu.activeSections.color,
+        lineCap: "round",
+        lineWidth: sizeToPixels[menu.activeSections.size]
+    }
+
+    ctx.beginPath()
+    _.forEach(attrs, function (value, name) {
+        ctx[name] = value;
+    });
+    
+    linePoints.forEach(function (point, ind) {
+       if (ind !== 0) {
+           var prevPoint = linePoints[ind - 1];
+           ctx.moveTo(prevPoint[0], prevPoint[1]);
+       }
+       ctx.lineTo(point[0], point[1]); 
+    });
+
+    ctx.stroke();
+    ctx.closePath();
+}
+
 $(document).ready( function(){
-    var c = $('#canvas');
+    var front = $(frontCanvas);
+    var back = $(backCanvas);
+
+    /*
     var ct = c.get(0).getContext('2d');
-    var container = $(c).parent();
+    */
+
+    var container = $(front).parent();
 
     //Run function when browser resizes
     $(window).resize( respondCanvas );
 
-    function respondCanvas(){ 
-        c.attr('width', $(container).width() ); //max width
-        c.attr('height', $(container).height() ); //max height
+    function respondCanvas(){
+        var w = $(container).width();
+        var h = $(container).height();
+        front.attr('width', w); //max width
+        front.attr('height', h); //max height
 
+        back.attr('width', w); //max width
+        back.attr('height', h); //max height
         //Call a function to redraw other content (texts, images etc)
     }
 
     //Initial call 
     respondCanvas();
-}); 
+});
 
 
+/*
+  How to write better d3 code:
+   understand how to grab data better
+   use the classed module more frequently
+   understand the whole api
+
+   I suspect all my d3 maps and forEacheas are bad code and could be done with selection and chaining + putting arguments on the data 
+   I should do a pass at rewriting...
+
+   What can and cannot be chained in d3?
+
+*/
